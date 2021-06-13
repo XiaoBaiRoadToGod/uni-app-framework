3cab2d00c015c11c06c8e7ad51d4e551929060ff
XiAOBaiRoadToGod <285857527@qq.com>
2021-06-13 13:37:18 +0800

    增加分包静态资源文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 // 自动构建h5 路由
 const TransformPages = require('uni-read-pages')
 const tfPages = new TransformPages({
@@ -12,7 +13,15 @@ module.exports = {
       // 自动构建H5 路由
       new tfPages.webpack.DefinePlugin({
         ROUTES: JSON.stringify(tfPages.routes)
-      })
+      }),
+
+      // 分包优化 图片
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, '/subpackageStatic'),
+          to: path.join(__dirname, '/dist/', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, '/')
+        }
+      ])
     ]
   }
 }
